1ebe0328f1ded32c15985f95e9966f9ccfdc8b83
caioreigot <caiocrrodrigues2004@gmail.com>
2023-02-22 14:44:28 -0300

    Teste (será removido)

--- backend/src/controllers/healthz/healthz.controller.ts
+++ backend/src/controllers/healthz/healthz.controller.ts
@@ -5,6 +5,7 @@ import { Response } from 'express';
 export class HealthzController {
   @Get('/healthz')
   healthz(@Res() response: Response) {
+    console.log('JWT_SECRET:', process.env.JWT_SECRET);
     response.status(HttpStatus.OK).send('O servidor está funcionando corretamente.');
   }
 }
