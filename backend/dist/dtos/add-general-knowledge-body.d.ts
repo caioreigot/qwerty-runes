e9e0ceaaff8235082f762bd879dbde47600043f6
caioreigot <caiocrrodrigues2004@gmail.com>
2023-02-22 13:02:12 -0300

    Pasta dist removida

--- backend/dist/dtos/add-general-knowledge-body.d.ts
+++
@@ -1,7 +0,0 @@
-import { GeneralKnowledgeQuestionType } from '../models/general-knowledge';
-export declare class AddGeneralKnowledgeBody {
-    questionTitle: string;
-    acceptableAnswers: string;
-    type: GeneralKnowledgeQuestionType;
-    content: string;
-}
