1226b48c59ed61080174f461c9e1e1ebaad55390
caioreigot <caiocrrodrigues2004@gmail.com>
2023-02-22 14:28:00 -0300

    Pasta dist removida

--- backend/dist/controllers/healthz/healthz.controller.d.ts
+++
@@ -1,4 +0,0 @@
-import { Response } from 'express';
-export declare class HealthzController {
-    healthz(response: Response): void;
-}
--- backend/dist/repositories/general-knowledge-repository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GeneralKnowledgeRepository = void 0;
-class GeneralKnowledgeRepository {
-}
-exports.GeneralKnowledgeRepository = GeneralKnowledgeRepository;
-//# sourceMappingURL=general-knowledge-repository.js.map
